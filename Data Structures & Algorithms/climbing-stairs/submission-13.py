8da2c64269407acb24005f86aa10f3123c0a0983
sunil220552 <sunil220552@users.noreply.github.com>
2026-04-27 08:27:01 -0700

    Add: climbing-stairs - submission-13

---
+++ Data Structures & Algorithms/climbing-stairs/submission-13.py
@@ -0,0 +1,22 @@
+import functools
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        ## Recursion meaning 
+        # helper(n) returns distric ways to reach top
+        # when we have n steps 
+        ## Base case 
+        # when n=0 zero ways 
+        # when n = 1, 1 way ; take one step 
+        # when n=2, two ways ; 1->1, 2
+        ## Explore 
+        # sum of ways to reach n-1 and n-2
+        @functools.lru_cache(maxsize=None)
+        def helper(n: int) -> int:
+            if n <= 2:
+                return n
+
+            return helper(n-1) + helper(n-2)
+
+        return helper(n)
+        
