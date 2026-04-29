76763fa8b867cd28f82b79e2085d3895f3322d13
sunil220552 <sunil220552@users.noreply.github.com>
2026-04-29 16:51:56 -0700

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.py
@@ -0,0 +1,33 @@
+import functools
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        ## Recursive definition 
+        # helper(i: int, curTotal: int) returns minimum number of coins required 
+        # to complete the total given the current total and coins[i:]
+        ## Base case
+        ## If i == len(coins), return 0. We dont have any conins 
+        ## if curTotal == amount, return 0. We don't need any more coins. 
+        ## Exploration 
+        # Explore the possible subset as an unbound knapsack. 
+        @functools.lru_cache(maxsize=None)
+        def helper(i: int, curTotal: int) -> int:
+
+            if curTotal == amount:
+                return 0
+            
+            if i == len(coins):
+                return float('inf')
+
+
+
+            res = helper(i+1, curTotal)
+
+            use = float('inf')
+
+            if curTotal + coins[i] <= amount:
+                res = min(res, 1 + helper(i, curTotal + coins[i]))
+
+            return res
+        res = helper(0, 0) 
+        return -1 if res == float('inf') else res    
